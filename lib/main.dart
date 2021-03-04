c4f2d3b1be99684fbbe9522a0912224c3f6c8331
danhausen <danihausen@live.com>
2021-03-04 20:36:31 -0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50,
@@ -37,6 +38,40 @@ class MyApp extends StatelessWidget {
                   letterSpacing: 2.5,
                 ),
               ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(color: Colors.teal.shade100),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+55 12 3456 7890',
+                    style: TextStyle(
+                        color: Colors.teal, fontFamily: 'Ubuntu', fontSize: 20),
+                  ),
+                ),
+              ),
+              Card(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.mail,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'danihausen@live.com',
+                      style: TextStyle(
+                          color: Colors.teal,
+                          fontFamily: 'Ubuntu',
+                          fontSize: 20),
+                    ),
+                  )),
             ],
           ),
         ),

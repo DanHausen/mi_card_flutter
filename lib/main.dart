feb6ccfa62c241a5a7ca455a3a6b1e4881be1007
danhausen <danihausen@live.com>
2021-03-03 22:50:43 -0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,36 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/eu.png'),
+                backgroundColor: Colors.transparent,
+              ),
+              Text(
+                "Dan Hausen",
+                style: TextStyle(
+                  fontFamily: 'Kalam',
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "FLUTTER / UNITY",
+                style: TextStyle(
+                  fontFamily: 'Ubuntu',
+                  color: Colors.teal.shade100,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                  letterSpacing: 2.5,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
